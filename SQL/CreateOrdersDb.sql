16387cab328d106b9c9d3f07a0c21a34a971e852
KorpinPoika <alexey.musti@gmail.com>
2023-03-08 18:08:44 +0300

    Changed russian text

--- SQL/CreateOrdersDb.sql
+++ SQL/CreateOrdersDb.sql
@@ -1,19 +1,19 @@
 /****** Object:  Database [OrdersDb]    Script Date: 3/2/2023 11:00:15 PM ******/
 CREATE DATABASE [OrdersDb]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'OrdersDb', FILENAME = N'/var/opt/mssql/data/OrdersDb.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'OrdersDb_log', FILENAME = N'/var/opt/mssql/data/OrdersDb_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
- WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
-GO
-ALTER DATABASE [OrdersDb] SET COMPATIBILITY_LEVEL = 160
-GO
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [OrdersDb].[dbo].[sp_fulltext_database] @action = 'enable'
-end
-GO
+-- CONTAINMENT = NONE
+-- ON  PRIMARY 
+--( NAME = N'OrdersDb', FILENAME = N'/var/opt/mssql/data/OrdersDb.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+-- LOG ON 
+--( NAME = N'OrdersDb_log', FILENAME = N'/var/opt/mssql/data/OrdersDb_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+-- WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+--GO
+--ALTER DATABASE [OrdersDb] SET COMPATIBILITY_LEVEL = 160
+--GO
+--IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+--begin
+--EXEC [OrdersDb].[dbo].[sp_fulltext_database] @action = 'enable'
+--end
+--GO
 ALTER DATABASE [OrdersDb] SET ANSI_NULL_DEFAULT OFF 
 GO
 ALTER DATABASE [OrdersDb] SET ANSI_NULLS OFF 
@@ -72,13 +72,13 @@ ALTER DATABASE [OrdersDb] SET TARGET_RECOVERY_TIME = 60 SECONDS
 GO
 ALTER DATABASE [OrdersDb] SET DELAYED_DURABILITY = DISABLED 
 GO
-ALTER DATABASE [OrdersDb] SET ACCELERATED_DATABASE_RECOVERY = OFF  
-GO
-EXEC sys.sp_db_vardecimal_storage_format N'OrdersDb', N'ON'
-GO
-ALTER DATABASE [OrdersDb] SET QUERY_STORE = ON
-GO
-ALTER DATABASE [OrdersDb] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+--ALTER DATABASE [OrdersDb] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+--GO
+--EXEC sys.sp_db_vardecimal_storage_format N'OrdersDb', N'ON'
+--GO
+--ALTER DATABASE [OrdersDb] SET QUERY_STORE = ON
+--GO
+--ALTER DATABASE [OrdersDb] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
 GO
 USE [OrdersDb]
 GO
@@ -93,7 +93,7 @@ CREATE TABLE [dbo].[Clients](
  CONSTRAINT [PK_Client] PRIMARY KEY CLUSTERED 
 (
 	[c_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 /****** Object:  Table [dbo].[Order_groups]    Script Date: 3/2/2023 11:00:16 PM ******/
@@ -107,7 +107,7 @@ CREATE TABLE [dbo].[Order_groups](
  CONSTRAINT [PK_Articul] PRIMARY KEY CLUSTERED 
 (
 	[a_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 /****** Object:  Table [dbo].[Orders]    Script Date: 3/2/2023 11:00:16 PM ******/
@@ -123,7 +123,7 @@ CREATE TABLE [dbo].[Orders](
  CONSTRAINT [PK_Order] PRIMARY KEY CLUSTERED 
 (
 	[o_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 SET IDENTITY_INSERT [dbo].[Clients] ON 
@@ -131,17 +131,17 @@ SET IDENTITY_INSERT [dbo].[Clients] ON
 INSERT [dbo].[Clients] ([c_id], [c_fio]) VALUES (CAST(1 AS Numeric(5, 0)), N'Ivanov Ivan Ivanovich')
 INSERT [dbo].[Clients] ([c_id], [c_fio]) VALUES (CAST(2 AS Numeric(5, 0)), N'Sidorov Sidor Sidorovich')
 INSERT [dbo].[Clients] ([c_id], [c_fio]) VALUES (CAST(3 AS Numeric(5, 0)), N'Petrov Petr Petrovich')
-INSERT [dbo].[Clients] ([c_id], [c_fio]) VALUES (CAST(4 AS Numeric(5, 0)), N'??????? ???? ?????????')
-INSERT [dbo].[Clients] ([c_id], [c_fio]) VALUES (CAST(5 AS Numeric(5, 0)), N'??? ????? ????????????')
+INSERT [dbo].[Clients] ([c_id], [c_fio]) VALUES (CAST(4 AS Numeric(5, 0)), N'Иванова Наталия Петровна')
+INSERT [dbo].[Clients] ([c_id], [c_fio]) VALUES (CAST(5 AS Numeric(5, 0)), N'Демьянова Ольга Игоревна')
 SET IDENTITY_INSERT [dbo].[Clients] OFF
 GO
 SET IDENTITY_INSERT [dbo].[Order_groups] ON 
 
-INSERT [dbo].[Order_groups] ([a_id], [a_name]) VALUES (CAST(1 AS Numeric(5, 0)), N'???????')
-INSERT [dbo].[Order_groups] ([a_id], [a_name]) VALUES (CAST(2 AS Numeric(5, 0)), N'??????')
-INSERT [dbo].[Order_groups] ([a_id], [a_name]) VALUES (CAST(3 AS Numeric(5, 0)), N'??????')
-INSERT [dbo].[Order_groups] ([a_id], [a_name]) VALUES (CAST(4 AS Numeric(5, 0)), N'????')
-INSERT [dbo].[Order_groups] ([a_id], [a_name]) VALUES (CAST(5 AS Numeric(5, 0)), N'???????')
+INSERT [dbo].[Order_groups] ([a_id], [a_name]) VALUES (CAST(1 AS Numeric(5, 0)), N'Самовар')
+INSERT [dbo].[Order_groups] ([a_id], [a_name]) VALUES (CAST(2 AS Numeric(5, 0)), N'Пила')
+INSERT [dbo].[Order_groups] ([a_id], [a_name]) VALUES (CAST(3 AS Numeric(5, 0)), N'Топор')
+INSERT [dbo].[Order_groups] ([a_id], [a_name]) VALUES (CAST(4 AS Numeric(5, 0)), N'Тяпка')
+INSERT [dbo].[Order_groups] ([a_id], [a_name]) VALUES (CAST(5 AS Numeric(5, 0)), N'Лопата')
 SET IDENTITY_INSERT [dbo].[Order_groups] OFF
 GO
 SET IDENTITY_INSERT [dbo].[Orders] ON 
@@ -225,5 +225,5 @@ order by o.o_datetime
 GO
 USE [master]
 GO
-ALTER DATABASE [OrdersDb] SET  READ_WRITE 
-GO
+--ALTER DATABASE [OrdersDb] SET  READ_WRITE 
+--GO
